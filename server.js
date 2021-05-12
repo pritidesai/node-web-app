899351f2890f07b189a7f8d46502fd799873a78d
Priti Desai <pdesai@us.ibm.com>
2021-05-12 11:59:26 -0700

    fixing the server.js to replace vars

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 
 // Constants
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || "8082";
 const HOST = process.env.HOST || "0.0.0.0";
 const VERSION = process.env.VERSION || "0.0";
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello RBC !!!!!!!  VERSION ${VERSION} ');
+  res.send('Hello RBC !!!!!!!  VERSION '+VERSION);
 });
 
 app.listen(PORT, HOST);
-console.log('Running on http://${HOST}:${PORT}');
+console.log('Running on http://%s:%s',HOST,PORT);
